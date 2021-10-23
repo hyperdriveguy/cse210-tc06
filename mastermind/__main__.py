f334a1177edf850bc1daa659c36c550c785e1469
brynlol <brynlol12@gmail.com>
2021-10-23 09:58:52 -0600

    Player and Roster Classes

--- mastermind/__main__.py
+++ mastermind/__main__.py
@@ -1,4 +1,9 @@
 from game.director import Director
 
+"""Starts the game.
+
+Create object "director" for the Director Class in director.py
+Call the start game method from Director Class"""
+
 director = Director()
 director.start_game()
