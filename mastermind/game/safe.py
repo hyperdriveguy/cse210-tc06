3ee8af02233b063f19d141ee90e82e745fee220b
brynlol <brynlol12@gmail.com>
2021-10-23 20:54:09 -0600

    finished adding and editing comments

--- mastermind/game/safe.py
+++ mastermind/game/safe.py
@@ -12,30 +12,54 @@ class Safe:
         Attributes:
             _code (string): four-digit code stored as a string
             _guess (string): four-digit guess stored as string
-            _players (list): list of players
-            _player_items (compound list): holds code, current guess, and hint for each player
+            _hint (string): hint for player stored as a string
        
        """
 
     def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Safe): an instance of Safe.
+        """
         self._code = ''
         self._guess = '----'
         self._hint = '****'
         
 
     def generate_code(self):
+        """Generates the code.
+        
+        Args:
+            self (Safe): An instance of Safe
+        """
         self._code = str(random.randint(1000, 9999))
 
     def get_code(self):
+        """Gets the code.
+        
+        Args:
+            self (Safe): An instance of Safe
+
+        Return (str): Returns the code as a string
+        """
         return self._code
 
     def apply_turn(self, turn):
-        """
+        """Executes a turn, setting the guess and hint
+
         Args:
-            turn (Turn): an instance of Turn
+            safe (Safe): an instance of Safe
         """
         self._guess = turn.get_guess()
         self._hint = turn.get_hint()
 
     def is_correct(self):
+        """Determine if the guess matches the code
+        
+        Args:
+            self (Safe): An instance of Safe
+
+        Return (bool): If guess matches code return true 
+        """
         return (self._guess == self._code)
